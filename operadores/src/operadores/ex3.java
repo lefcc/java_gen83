f2479409e3bfa554c1678cd9fe989ffe54502c70
LeticiaC <leticiafccampos@gmail.com>
2025-09-14 20:45:38 -0300

    Resolução exercício 3 - Java 5.3

--- operadores/src/operadores/ex3.java
+++ operadores/src/operadores/ex3.java
@@ -1,10 +1,30 @@
 package operadores;
+import java.util.Scanner;
+
 
 public class ex3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		float salario = 0, adicional = 0, horaExtra = 0, descontos = 0;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("Salário Bruto: ");
+		salario = input.nextFloat();
+		
+		System.out.printf("Adicional Noturno: ");
+		adicional = input.nextFloat();
+		
+		System.out.printf("Horas Extras: ");
+		horaExtra = input.nextFloat();
+		
+		System.out.printf("Descontos: ");
+		descontos = input.nextFloat();
+		
+		System.out.printf("Salário Líquido: %.2f", salario + adicional + (horaExtra*5) - descontos);
+		
+		input.close();
+		
 	}
 
 }
