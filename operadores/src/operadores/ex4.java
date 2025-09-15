d9c69af2b024474af314b71bd00b2d233287b78e
LeticiaC <leticiafccampos@gmail.com>
2025-09-14 22:30:59 -0300

    Resolução exercício 4 - Java 5.3

--- operadores/src/operadores/ex4.java
+++ operadores/src/operadores/ex4.java
@@ -1,10 +1,25 @@
 package operadores;
+import java.util.Scanner;
 
 public class ex4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		float [] numero = new float[4];
+		int i;
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		for (i=0; i<4; i++) {
+		System.out.printf("numero%d: ", i+1);
+		numero[i] = input.nextFloat();
+				
+		}
+		
+		System.out.printf("Diferença: %.1f", (numero[0] * numero[1]) - (numero[2] * numero[3]));
+		
+		input.close();
 	}
 
 }
